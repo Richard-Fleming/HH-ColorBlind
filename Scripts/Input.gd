7bac2e94e28b6b05afc4ca98aa9e3078135f9a15
Richard Fleming <richard.fleming164@gmail.com>
2022-07-27 14:35:42 +0100

    Updated hierarchy for ease of use, adjusted UI, Added Verdict Visuals to the endscreen, Added dedicated Pass Button during gameplay

--- Scripts/Input.gd
+++ Scripts/Input.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var currentNum = ""
 var sequence = ["Blind 45", "5", "6", "7", "15", "26"]
+var rightWrong = [0,0,0,0,0,0]
 var sequenceIterator = 0
 var blindPlateTex = preload("res://Sprites/Blind_Plate.png")
 var fivePlateTex = preload("res://Sprites/5_Plate.png")
@@ -9,12 +10,16 @@ var sixPlateTex = preload("res://Sprites/6_Plate.png")
 var sevenPlateTex = preload("res://Sprites/7_Plate.png")
 var fifteenPlateTex = preload("res://Sprites/15_Plate.png")
 var twentySixPlateTex = preload("res://Sprites/26_Plate.png")
+var rightText = preload("res://Sprites/Tick.png")
+var wrongText = preload("res://Sprites/Cross.png")
 var numPlate
 var displayedNum
 var displayedTimer
 var inputUIGroup
 var endUI
 var menuUI
+var gameplayControl
+var finalPlates
 var verdict = 0
 var answered = false
 export var maxTime: float = 15
@@ -23,32 +28,35 @@ var endTest = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	inputUIGroup = get_node("InputUI")
-	inputUIGroup.visible = false
-	numPlate = get_node("Plate")
-	numPlate.visible = false
-	displayedNum = get_node("InputUI/DisplayedNum")
-	displayedTimer = get_node("Timer")
-	displayedTimer.visible_characters = 5
-	displayedTimer.visible = false
+	gameplayControl = get_node("Gameplay")
+	gameplayControl.visible = false
+	inputUIGroup = get_node("Gameplay/InputUI")
+	numPlate = get_node("Gameplay/Plate")
+	displayedNum = get_node("Gameplay/InputUI/DisplayedNum")
+	displayedTimer = get_node("Gameplay/Timer")
 	endUI = get_node("EndUI")
 	endUI.visible = false
 	endUI.get_node("RestartButton").set_process(false)
 	endUI.get_node("MenuReturn").set_process(false)
 	menuUI = get_node("MainMenu")
 	menuUI.visible = true
+	finalPlates = get_node("EndUI/FinalPlates")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if sequenceIterator > 5 && !endTest:
-		inputUIGroup.visible = false
-		numPlate.visible = false
-		displayedTimer.visible = false
+		gameplayControl.visible = false
+		gameplayControl.set_process(false)
 		endUI.visible = true
 		endUI.get_node("DisplayedVerdict").text = "Verdict: " + String(verdict) + "/6"
 		endUI.get_node("RestartButton").set_process(true)
 		endUI.get_node("MenuReturn").set_process(true)
+		for i in rightWrong.size():
+			if rightWrong[i] == 1:
+				finalPlates.get_child(i).get_node("RightWrong").set_texture(rightText)
+			else:
+				finalPlates.get_child(i).get_node("RightWrong").set_texture(wrongText)
 		endTest = true
 	if answered == true && sequenceIterator < 6:
 		answered = false
@@ -71,7 +79,7 @@ func _process(delta):
 		answered = true
 		_check_answer()
 		_next_in_sequence()
-	displayedTimer.text = String(timer)
+	displayedTimer.text =  String("%.2f" % timer)
 
 
 func _next_in_sequence():
@@ -84,21 +92,27 @@ func _check_answer():
 		"Blind 45":
 			if(currentNum == ""):
 				verdict += 1
+				rightWrong[0] = 1
 		"5":
 			if(currentNum == "5"):
 				verdict += 1
+				rightWrong[1] = 1
 		"6":
 			if(currentNum == "6"):
 				verdict += 1
+				rightWrong[2] = 1
 		"7":
 			if(currentNum == "7"):
 				verdict += 1
+				rightWrong[3] = 1
 		"15":
 			if(currentNum == "15"):
 				verdict += 1
+				rightWrong[4] = 1
 		"26":
 			if(currentNum == "26"):
 				verdict += 1
+				rightWrong[5] = 1
 
 # Called whenever a button is pressed in the Numpad during the test.
 func _on_Button_button_down(extra_arg_0: String):
@@ -127,6 +141,12 @@ func _on_Button_button_down(extra_arg_0: String):
 				currentNum += "0"
 	if extra_arg_0 == "BackButton":
 		currentNum.erase(currentNum.length() - 1, 1)
+	if extra_arg_0 == "PassButton":
+		answered = true
+		if sequence[sequenceIterator] == "Blind 45":
+			verdict += 1
+			rightWrong[0] = 1
+		_next_in_sequence()
 	if extra_arg_0 == "EnterButton":
 		answered = true
 		_check_answer()
@@ -136,9 +156,8 @@ func _on_Button_button_down(extra_arg_0: String):
 
 
 func _on_Restart_button_down():
-	inputUIGroup.visible = true
-	numPlate.visible = true
-	displayedTimer.visible = true
+	gameplayControl.visible = true
+	gameplayControl.set_process(true)
 	endUI.visible = false
 	endUI.get_node("RestartButton").set_process(false)
 	endUI.get_node("MenuReturn").set_process(false)
@@ -147,6 +166,8 @@ func _on_Restart_button_down():
 	sequenceIterator = 0
 	timer = maxTime
 	endTest = false
+	for i in rightWrong.size():
+		rightWrong[i] = 0
 	randomize()
 	sequence.shuffle()
 	match sequence[sequenceIterator]:
